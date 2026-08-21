58434f5ed6fd8b18b840af726c73a7912ba4f0aa
parth-veno <164373072+parth-veno@users.noreply.github.com>
2026-08-21 04:16:15 +0000

    close and open

--- script.js
+++ script.js
@@ -45,3 +45,19 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+var welcomeScreen = document.querySelector("#mydiv");
+function closeWindow(element) {
+  element.style.display = "none"
+}
+function openWindow(element) {
+  element.style.display = "flex"
+}
+var welcomeScreenClose = document.querySelector("#welcomeclose");
+var welcomeScreenOpen = document.querySelector("#welcomeopen");
+welcomeScreenClose.addEventListener("click", function() {
+  closeWindow(welcomeScreen);
+});
+welcomeScreenOpen.addEventListener("click", function() {
+  openWindow(welcomeScreen);
+});
